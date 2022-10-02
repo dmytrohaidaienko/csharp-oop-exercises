f465ad77e01abd30d4f46f800c48695d19f5a023
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 17:20:24 +0300

    Added task for exercise №1.

---
+++ exercise_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    /*
+    Задание №1: Абстрактные классы, наследование и полиморфизм.
+    1. Создайте класс Dog. 
+    2.Наследник Animal.
+    3. Реализуйте в нем полиморфный метод eat(), возвращающий строку "Bone".
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
