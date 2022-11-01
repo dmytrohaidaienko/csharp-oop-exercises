2a192a30936d2018ae435cc04bcc08d3cb4b0137
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-11-01 23:01:52 +0200

    Added task for exercise №2.

---
+++ exercise_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+
+    /*
+    Задание №2:
+    В пошаговой стратегии каждый юнит может атаковать.
+    Стандартный юнит атакует мечом. Но есть еще два типа юнитов — мушкетеры и маги, атакующие по-своему.
+    Напишите программу, которая объявляет класс Unit, который имеет метод Attack(), а он выводит "Using sword!".
+    Унаследуйте классы Musketeer и Magician от класса Unit и переопределите его метод Attack() для вывода соответствующих сообщений во время атаки:
+    Musketeer => "Using musket!"
+    Magician =>"Using magic!"
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
