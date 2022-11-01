848857a5b88a85775e641ebb7bd21dbbecca212d
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-11-02 00:12:52 +0200

    Added task for exercise №4.

---
+++ exercise_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_4
+{
+
+    /*
+    Задание №4:
+    Создать класс Tiles (кафель), который будет содержать поля с открытым доступом: brand, size_h, size_w, price и метод класса getData(). 
+    В главной функции объявить пару объектов класса и внести данные в поля. Затем отобразить их, вызвав метод getData().
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
