dbb1916cd1fb4c0713b5d1236c1cb96305d343e1
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-11-06 15:45:23 +0200

    Added task for exercise №5.

---
+++ exercise_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_5
+{
+
+    /*
+    Задание №5:
+    Программа, которую вы должны создать, должна принимать марку и модель автомобиля в качестве входных данных и определять класс Vehicle с методом ShowModel().
+    Напишите класс Car, чтобы он наследовал класс Vehicle, и добавьте свойство Model и метод ShowModel(), чтобы заданный вызов метода объекта car работал правильно.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
