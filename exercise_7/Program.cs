02f0ad26cc08aac6017b5dc69da939ea9338463a
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-11-06 17:33:57 +0200

    Added task for exercise №7.

---
+++ exercise_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_7
+{
+
+    /*
+    Задание №7:
+    Написать программу, которая определяет абстрактный класс Figure, и наследует от него классы Rectangle и Triangle. 
+    Добавьте к классу Figure абстрактный метод Perimeter, затем переопределите его в унаследованных классах для расчета периметров объектов Rectangle и Triangle (они тоже уже созданы). 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
